2c48821eb61e9d1824dbf0788f297b03521810b9
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-04-21 13:35:52 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <div className="min-h-screen flex justify-center items-center">
-        <div className='bg-gray-100 w-10/12 p-10 rounded-xl'>
+        <div className='bg-gray-100 w-11/12 lg:w-10/12 p-10 rounded-xl'>
           <div className='flex flex-col gap-1'>
             <DropdownTable assetName="Real Estate" />
             <DropdownTable assetName="Cash" />
